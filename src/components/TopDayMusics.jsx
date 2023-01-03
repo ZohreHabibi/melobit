3a3db87a42fedf48a7a14de3871c192910c8139e
Zohre Habibi <hzhrh817@gmail.com>
2023-01-03 21:13:40 +0330

    implementing top week music and top day music

--- src/components/TopDayMusics.jsx
+++ src/components/TopDayMusics.jsx
@@ -1,15 +1,17 @@
-import { useGetTopDayMusicsQuery } from "../redux/services/shazamCoreApi";
-import Loading from "./Loading";
-import TrendingMusicCard from "./TrendingMusicCard";
+import { useGetTopDayMusicsQuery } from "../redux/services/melobitApi";
+import Error from "./Error";
+import Loader from "./Loader";
+import MusicCard from "./MusicCard";
 
 const TopDayMusics = () => {
   const { data, error, isLoading } = useGetTopDayMusicsQuery();
   if (isLoading) {
-    return <Loading />;
+    return <Loader title="Top Day Musics" />;
   }
   if (error) {
-    return <p>Error</p>;
+    return <Error />;
   }
+
   return (
     <div className="mt-12 mx-10">
       <h2 className="text-white md:ml-[7rem] lg:ml-0 text-2xl">
@@ -17,7 +19,13 @@ const TopDayMusics = () => {
       </h2>
       <div className="flex justify-center items-center flex-wrap">
         {data.results.slice(0, 4).map((music, i) => (
-          <TrendingMusicCard key={music.id} i={i} music={music} />
+          <MusicCard
+            currentListData={data.results.slice(0, 4)}
+            type="trending"
+            key={music.id}
+            i={i}
+            music={music}
+          />
         ))}
       </div>
     </div>
--- src/components/TopWeekMusics.jsx
+++ src/components/TopWeekMusics.jsx
@@ -1,23 +1,30 @@
-import { useGetTopWeekMusicsQuery } from "../redux/services/shazamCoreApi";
-import Loading from "./Loading";
-import TrendingMusicCard from "./TrendingMusicCard";
+import { useGetTopWeekMusicsQuery } from "../redux/services/melobitApi";
+import Error from "./Error";
+import Loader from "./Loader";
+import MusicCard from "./MusicCard";
 
 const TopWeekMusics = () => {
   const { data, error, isLoading } = useGetTopWeekMusicsQuery();
-  console.log(data);
 
   if (isLoading) {
-    return <Loading />;
+    return <Loader title="Top Week Musics" />;
   }
   if (error) {
-    return <p>Error</p>;
+    return <Error />;
   }
+
   return (
     <div className="mt-12 mx-10">
       <h2 className="text-white text-2xl">Week Trending</h2>
       <div className="flex justify-center items-center flex-wrap">
         {data.results.slice(0, 4).map((music, i) => (
-          <TrendingMusicCard key={music.id} i={i} music={music} />
+          <MusicCard
+            currentListData={data.results.slice(0, 4)}
+            type="trending"
+            key={music.id}
+            i={i}
+            music={music}
+          />
         ))}
       </div>
     </div>
